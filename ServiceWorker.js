86055e0b2f17055c104a59db255ce6a3cfbd22ba
Trazendente <trazendente.studio@gmail.com>
2024-11-27 18:48:31 -0500

    Initial commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Trazendente-Hada Supermaxi-0.1";
+const contentToCache = [
+    "Build/HadaV1.loader.js",
+    "Build/HadaV1.framework.js",
+    "Build/HadaV1.data",
+    "Build/HadaV1.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
